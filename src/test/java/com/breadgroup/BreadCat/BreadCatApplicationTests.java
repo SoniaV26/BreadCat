9fea26da1da1181511377a2f0ddcbc0d93f736ef
aparmar2000 <aparmar2000@gmail.com>
2019-11-08 10:53:35 -0700

    Initial Springboot Project

---
+++ src/test/java/com/breadgroup/BreadCat/BreadCatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breadgroup.BreadCat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreadCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
