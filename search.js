6be4d8435468bf465e9d204b413f49dac6d3a2dc
aparmar2000 <aparmar2000@gmail.com>
2019-11-24 15:38:47 -0700

    Added filtering capability to search function.
    May not function, depending on exact db setup.
    Will most likly require minor tweaking once actual db is available.

--- search.js
+++ search.js
@@ -1,26 +1,72 @@
 const sqlite = require("sqlite");
 /*
-* resultArray: searchRestaurants(string: searchQuery, database: dbPromise)
+* resultArray: searchRestaurants(string: searchQuery, database: dbPromise,
+* 								boolean: glutenFreeFilter,
+*								boolean: vegetarianFilter,
+*								boolean: veganFilter,
+*								boolean: lowCalorieFilter,
+*								boolean: kosherFilter)
 */
-function searchRestaurants(searchQuery, dbPromise) {
+function searchRestaurants(searchQuery, dbPromise,
+						glutenFreeFilter,
+						vegetarianFilter,
+						veganFilter,
+						lowCalorieFilter,
+						kosherFilter) {
 	searchQuery = "%"+searchQuery+"%"
 	const db = await dbPromise;
 	var resultArray;
 	var index = 0;
-	var query = "SELECT Name name,
-                  Description description
+	var filterMatches, filterCount;
+	var restrictionNames = ["Gluten-free", "Vegetarian",
+							"Vegan","Low sugar/calories",
+							"Kosher"];
+	var queryRestaurant = "SELECT Name name,
+                  Description description,
+				  Accomodations accomodations
 				FROM Restaurant
 				WHERE Name LIKE ${searchQuery} OR
 				  Description LIKE ${searchQuery}";
+	var queryRestriction = "SELECT Restriction restriction
+				FROM DietaryRestriction
+				WHERE RestrictionID = ?";
 	
-	db.each(query, (err, row) => {
+	filterCount += glutenFreeFilter;
+	filterCount += vegetarianFilter;
+	filterCount += veganFilter;
+	filterCount += lowCalorieFilter;
+	filterCount += kosherFilter;
+	
+	await db.each(queryRestaurant, (err, restaurantRow) => {
 	  if (err) {
 		throw err;
 	  }
 	  
-	  resultArray[index].name = row.name;
-	  resultArray[index].description = row.description;
-	  index++;
+	  db.each(queryRestriction, restaurantRow.Accomodations, (err, restrictionRow) => {//Filter by dietary restrictions
+		  switch (restrictionRow.restriction) {
+			  case restrictionNames[0]:
+				filterMatches += glutenFreeFilter;
+				break;
+			  case restrictionNames[1]:
+				filterMatches += vegetarianFilter;
+				break;
+			  case restrictionNames[2]:
+				filterMatches += veganFilter;
+				break;
+			  case restrictionNames[3]:
+				filterMatches += lowCalorieFilter;
+				break;
+			  case restrictionNames[4]:
+				filterMatches += kosherFilter;
+				break;
+		  }
+	  });
+	  
+	  if (filterMatches >= filterCount) {//Add restaurant data to array
+		  resultArray[index].name = restaurantRow.name;
+		  resultArray[index].description = restaurantRow.description;
+		  index++;
+	  }
 	});
 	
 	return resultArray;
