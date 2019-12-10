f487cad5728828cb2194ef9b501bb4ef525c65a2
HANtheSassQueen <henry.newsum@gmail.com>
2019-12-10 12:47:32 -0700

    Added 'gallery' to database
    gallery stores an id, restaurant id FK, user id FK, and relative file path to image. image.handlebars page is created, and there should be a get and post for the image in the JS(Not functional yet)

--- breadcat/BreadCat-/migrations/001-initial-schema.sql
+++ breadcat/BreadCat-/migrations/001-initial-schema.sql
@@ -76,6 +76,15 @@ CREATE TABLE rest_diet (
     accomodate STRING,
     FOREIGN KEY (restId) REFERENCES restaurant(id)
 );
+
+CREATE TABLE gallery (
+    id INTEGER PRIMARY KEY,
+    restId INTEGER,
+    userId INTEGER,
+    imgFilePath STRING,
+    FOREIGN KEY (restId) REFERENCES restaurant(id),
+    FOREIGN KEY (userId) REFERENCES user(id)
+);
 */
 
 -- Down
